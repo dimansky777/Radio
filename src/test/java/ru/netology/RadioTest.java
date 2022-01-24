cde25a52898f632bc85b5be5cfaeceab9ef162c6
Dmitriy Tikhonin <dimansky777@gmail.com>
2022-01-24 10:06:12 +0300

    Tests updated

--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -19,6 +19,32 @@ class RadioTest {
     }
 
     @Test
+    //Переключение радио от 0 до 9  return test 1
+    public void testReturnPositionBelowZero() {
+        Radio rad = new Radio();
+        rad.setCurrentRadioStation(-1);
+
+
+        int expected = 0;
+        int actual = rad.getCurrentRadioStation();
+        assertEquals (expected, actual);
+
+    }
+    @Test
+    //Переключение радио от 0 до 9  return test 2
+    public void testReturnPositionOver9() {
+        Radio rad = new Radio();
+        rad.setCurrentRadioStation(11);
+
+
+        int expected = 0;
+        int actual = rad.getCurrentRadioStation();
+        assertEquals (expected, actual);
+
+    }
+
+
+    @Test
     //Кнопка Next
     public void setNextRadioSation(){
     Radio rad = new Radio();
@@ -51,10 +77,10 @@ class RadioTest {
     public void setVolumeUp(){
         Radio rad = new Radio();
 
-        rad.setCurrentVolume(10);
+        rad.setCurrentVolume(0);
         rad.setVolumeUp();
 
-        int expected = 10;
+        int expected = 1;
         int actual = rad.getCurrentVolume();
         assertEquals(expected,actual);
     }
@@ -72,4 +98,33 @@ class RadioTest {
         int actual = rad.getCurrentVolume();
         assertEquals(expected,actual);
     }
+
+
+    @Test
+    // Volume test return
+    public void setVolumeBelowZero(){
+        Radio rad = new Radio();
+
+        rad.setCurrentVolume(-1);
+        //rad.setVolumeDown();
+
+        int expected = 0;
+        int actual = rad.getCurrentVolume();
+        assertEquals(expected,actual);
+    }
+
+
+    @Test
+    // Volume test return
+    public void setVolumeAboveTen(){
+        Radio rad = new Radio();
+
+        rad.setCurrentVolume(11);
+        //rad.setVolumeDown();
+
+        int expected = 0;
+        int actual = rad.getCurrentVolume();
+        assertEquals(expected,actual);
+    }
+
 }
